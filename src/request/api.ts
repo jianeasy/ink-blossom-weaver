24bae8f58475bb2e0fa71f72ec7deb53caa7864c
jianyi <jianyi@codejoy.ai>
2025-05-19 10:17:03 +0800

    feat(api): 新增保存、收藏、取消收藏及获取收藏列表的API接口
    新增了四个API接口，分别用于保存图像、收藏图像、取消收藏以及获取收藏列表。这些接口支持用户操作和记录收藏行为，提升用户体验。

--- src/request/api.ts
+++ src/request/api.ts
@@ -19,3 +19,67 @@ export const generateImageApi = (data: {
     },
   });
 };
+
+//保存
+export const saveImageApi = (data: {
+  toolName: string;
+  toolType: string;
+  setting: string;
+}) => {
+  const userId = localStorage.getItem("userId") || "test1";
+  return req.request({
+    method: "post",
+    url: "https://aitool.codejoyai.com/aitool_api/tool/createLog",
+    // url: "http://localhost:4040/api/tool/createLog",
+    data: data,
+    headers: {
+      platform: "lndx",
+      userId: userId,
+    },
+  });
+};
+
+// 收藏
+export const collectImageApi = (data: { uuid: string }) => {
+  return req.request({
+    method: "post",
+    // url: "https://aitool.codejoyai.com/aitool_api/tool/collection",
+    url: "http://localhost:4040/api/tool/collection",
+    data: data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+// 取消收藏
+export const cancelCollectImageApi = (data: { uuid: string }) => {
+  return req.request({
+    method: "post",
+    url: "https://aitool.codejoyai.com/aitool_api/tool/cancelCollection",
+    // url: "http://localhost:4040/api/tool/cancelCollection",
+    data: data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+// 获取收藏的列表
+export const getCollectImageListApi = (data: {
+  page: number;
+  pageSize: number;
+  toolName: string;
+}) => {
+  const userId = localStorage.getItem("userId") || "test1";
+  return req.request({
+    method: "post",
+    url: "https://aitool.codejoyai.com/aitool_api/tool/collectionList",
+    // url: "http://localhost:4040/api/tool/collectionList",
+    data: data,
+    headers: {
+      platform: "lndx",
+      userId: userId,
+    },
+  });
+};
